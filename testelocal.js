89ce694a18983ce15dd4296daed5d18ed7711636
Guilherme Somenzi <guilherme@forit.tech>
2017-12-20 09:31:39 -0200

    Adding SzIfaceConfigurator getInterfaces and getAddresses

--- testelocal.js
+++ testelocal.js
@@ -4,13 +4,13 @@ let options = {
   getIpData: false, 
   sudo: false, 
   args: [], 
-  exclude: ["vethaff756e@if45"]
+  exclude: []
 }
 SzIFaceConfig.getInterfaces('all', options, (err, interfaces) => {
   if (err) return console.log(err)
   return console.log(JSON.stringify(interfaces))
 })
 SzIFaceConfig.getAddresses('eno1', options, (err, addresses) => {
-  if (err) return console.log(err)
-  return console.log(addresses)
+ if (err) return console.log(err)
+ return console.log(addresses)
 })
